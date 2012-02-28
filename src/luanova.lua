196338e0dfe660fc2c956ff9750126330d1faa35
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-02-28 18:05:29 -0300

    New ideas, CMake stub and doc update.

--- src/luanova.lua
+++ src/luanova.lua
@@ -1,16 +1,18 @@
---- Main LuaNova file.
+--- Main LuaNova module.
 -- File version: 0.0.0
+-- @autor Kazuo256
+module("nova", package.seeall)
 
---- The LuaNova module.
+--- Contains information about LuaNova nova's module.
 -- @field version The version of this LuaNova module.
-luanova = {}
-
-luanova.version = "0.0.0"
+info = {
+  version = "0.0.0"
+}
 
 
 --- Prints all key-value pairs of the given table to the standard output.
 -- @param t The table whose field are to be listed.
-function luanova.ls(t)
+function ls(t)
   for k,v in pairs(t) do
     print(k,v)
   end
