c5e511e1026b3415afe427bf6caef6f2396dac24
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-02-28 22:49:43 -0300

    Developing CMake skeleton.

--- cmake/luanova_module.cmake
+++ cmake/luanova_module.cmake
@@ -1,7 +1,21 @@
+################################################################################
+## LuaNova Project by Kazuo 256
+## File: cmake/luanova_module.cmake
+## Description:
+##    Responsible for adding LuaNova's modules as targets to be built.
+################################################################################
 
+## Needs LuaDoc to automatically generate the modules' documentation.
 find_program (LUADOC luadoc)
 
-function (LUANOVA_ADD_MODULES modules)
+if (LUADOC STREQUAL LUADOC-NOTFOUND)
+  message ("-- !!WARNING!! LuaDoc not found, no documentation will be "
+           "generated.")
+else (LUADOC STREQUAL LUADOC-NOTFOUND)
+  message ("-- Using LuaDoc: ${LUADOC}")
+endif (LUADOC STREQUAL LUADOC-NOTFOUND)
+
+function (luanova_add_modules modules)
   #add_custom_command
-endfunction (LUANOVA_ADD_MODULES)
+endfunction (luanova_add_modules)
 
