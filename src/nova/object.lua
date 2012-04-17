f0465d0a636738bea30106a8ad2a153c994ea92c
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-04-17 07:58:25 -0300

    Changing object approach.

--- src/nova/object.lua
+++ src/nova/object.lua
@@ -22,13 +22,14 @@ do
   --- Creates a new object from a prototype.
   -- @param prototype A table containing the objects methods and metamethods,
   --                  along with the default values of its attributes.
-  function meta:__call (prototype)
-    local new_obj = {}
-    prototype.__index = prototype.__index or prototype
-    prototype.__proto = prototype
-    setmetatable(new_obj, prototype)
-    return new_obj
+  function meta:__call (obj)
+    obj = obj or {}
+    self.__index = self
+    setmetatable(obj, self)
+    return obj
   end
+
+  __call = meta.__call
 
 end
 
