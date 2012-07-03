a39085f805b0066ebf0db97948258c331773ab94
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-07-03 10:20:36 -0300

    Small refactorings.

--- src/nova/common.lua
+++ src/nova/common.lua
@@ -6,9 +6,7 @@ do
   --- Prints all key-value pairs of the given table to the standard output.
   -- @param t The table whose field are to be listed.
   function ls (t)
-    for k,v in pairs(t) do
-      print(k,v)
-    end
+    table.foreach(t, print)
   end
   
   --- Converts an array to an argument list.
