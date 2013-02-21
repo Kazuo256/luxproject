ff1390f9c3988a93544e74ee8aa123f0af2658b1
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-02-21 17:59:34 -0300

    [functional] Added the chain(f,n) function.

--- lux/functional.lua
+++ lux/functional.lua
@@ -18,5 +18,45 @@ module ("lux.functional", package.seeall) do
     end
   end
 
+  --- Creates a <code>n</code>-chained function based on <code>f</code>.
+  -- <p>
+  --  Chained functions receive their arguments in consecutive calls. For
+  --  instance, if <code>f</code> was the usual <code>print</code> and
+  --  <code>n</code> was 1, you would use the resulting function like this:
+  -- </p>
+  -- <p><code>
+  -- local result = chain(print,1)
+  -- </code></p>
+  -- <p><code>
+  -- result (arg1) (arg2, arg3, ...)
+  -- </code></p>
+  -- <p>
+  --  Using <code>n</code> was 2, then the call would be:
+  -- </p>
+  -- <p><code>
+  -- result (arg1) (arg2) (arg3, arg4, ...)
+  -- </code></p>
+  -- <p>
+  --  And so on.
+  -- </p>
+  -- @param f The function being chained.
+  -- @param n The size of the chain.
+  -- @return An <code>n</code>-chained function version of <code>f</code>.
+  function chain (f, n)
+    n = n or 1
+    return function (...)
+      local first, second = ...
+      if n >= 1 and not second then
+        if first then
+          return simplechain(bindleft(f, first), n-1)
+        else
+          return simplechain(f, n)
+        end
+      else
+        return f(...)
+      end
+    end
+  end
+
 end
 
