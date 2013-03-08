bcacf5d8cbba5ccce790feb3811c8ebebf6fe267
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2013-03-08 01:04:44 -0300

    [lux.functional] Fixed bindleft issues and added bindfirst.

--- lux/functional.lua
+++ lux/functional.lua
@@ -3,18 +3,32 @@
 -- Some functional programming tools lay around here.
 module ("lux.functional", package.seeall) do
 
+  --- Binds a function's first parameter to the given argument.
+  -- @param f   The function being binded.
+  -- @param arg The bound argument.
+  -- @return A function that, upon being called, does the same as f, but
+  --         requires only the arguments beyond the first one.
+  function bindfirst (f, arg)
+    local up = arg
+    return function (...)
+      return f(up, ...)
+    end
+  end
+
   --- Binds a function to the given (left-most) arguments.
   -- The arguments must be passed in the apropriate order, according to the
   -- function's specification.
-  -- @param f The function being binded.
-  -- @param ... The bound arguments, in order.
+  -- @param f     The function being binded.
+  -- @param arg1  The first bound argument.
+  -- @param ...   The remaining bound arguments, in order.
   -- @return A function that, upon being called, does the same as f, but
   --         requires only the remaining right-most arguments that were not
   --         binded with it.
-  function bindleft (f, ...)
-    local args = { ... }
-    return function (...)
-      return f(unpack(args), ...)
+  function bindleft (f, arg1, ...)
+    if select('#', ...) == 0 then
+      return bindfirst(f, arg1)
+    else
+      return bindleft(bindfirst(f, arg1), ...)
     end
   end
 
