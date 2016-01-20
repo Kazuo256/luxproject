b0d60ba97dda5d4629dc61227abc61abaaedee05
Kazuo256 <kazuo256@gmail.com>
2016-01-20 20:05:45 -0200

    lux.macro.Processor
    Code style review

--- lib/lux/macro/Processor.lua
+++ lib/lux/macro/Processor.lua
@@ -68,7 +68,8 @@ function Processor:process (instream, outstream, env)
     count = count + step
   end
   code = code .. [[output:write ]] .. "[[\n" .. str:sub(count) .. "]]\n"
-  portable.loadWithEnv(assert(load(code)), makeDirectiveEnvironment(outstream, env)) ()
+  portable.loadWithEnv(assert(load(code)),
+                       makeDirectiveEnvironment(outstream, env)) ()
 end
 
 return Processor
